e02c89d66865539feb015108182105541e43d553
jibankumarpanda <pandajiban331@gmail.com>
2026-06-21 23:24:12 +0530

    q1 of 2d array

--- 2darray/q1.c
+++ 2darray/q1.c
@@ -1,2 +1,12 @@
 // write a program to store the roll number and marks obtained by 4 students side by side in a matrix.
 #include<stdio.h>
+int main(){
+    int arr[4][2]={{1, 85}, {2, 90}, {3, 78}, {4, 92}};
+    for(int i=0;i<4;i++){
+       for(int j=0;j<2;j++){
+           printf("%d ", arr[i][j]);
+       }
+       printf("\n");
+    }
+    return 0;
+}
